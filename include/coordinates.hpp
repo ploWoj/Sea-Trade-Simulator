605dffe21fd504c231c7fd135871329d46acbf14
Wojtek <wojtek.plociennik@gmail.com>
2023-03-31 11:47:44 -0700

    fixing operato==

--- include/coordinates.hpp
+++ include/coordinates.hpp
@@ -15,9 +15,10 @@ public:
     constexpr void setPositionY(size_t positionY) { positionY_ = positionY; };
     constexpr size_t getPositionY() const { return positionY_; };
 
-    constexpr bool operator== (const Coordinates& n_pos) const {
-        return positionX_ == n_pos.getPositionX() && positionY_ == n_pos.getPositionY();
-    };
+    constexpr bool operator== (const Coordinates& n_pos) const ;
+    // {
+    //     return positionX_ == n_pos.getPositionX() && positionY_ == n_pos.getPositionY();
+    // };
 
     constexpr size_t distance(const Coordinates& rhs) { 
                 return std::round(std::sqrt(std::pow((static_cast<int>(positionX_) - static_cast<int>(rhs.getPositionX())), 2) +
--- src/coordinates.cpp
+++ src/coordinates.cpp
@@ -1,32 +1,32 @@
-// #include "coordinates.hpp"
+#include "../include/coordinates.hpp"
 
-// void Coordinates::setPositionX(size_t positionX)
-// {
-//     positionX = positionX_;
-// }
-// size_t Coordinates::getPositionX() const
-// {
-//     return positionX_;
-// }
+void Coordinates::setPositionX(size_t positionX)
+{
+    positionX = positionX_;
+}
+size_t Coordinates::getPositionX() const
+{
+    return positionX_;
+}
 
-// void Coordinates::setPositionY(size_t positionY)
-// {
-//     positionY = positionY_;
-// }
-// size_t Coordinates::getPositionY() const
-// {
-//     return positionY_;
-// }
+void Coordinates::setPositionY(size_t positionY)
+{
+    positionY = positionY_;
+}
+size_t Coordinates::getPositionY() const
+{
+    return positionY_;
+}
 
-// bool Coordinates::operator==(const Coordinates n_pos) const
-// {
-//     return positionX_ == n_pos.positionX_ && positionY_ == n_pos.positionY_;
-// }
+bool Coordinates::operator==(const Coordinates& n_pos) const
+{
+        return positionX_ == n_pos.getPositionX() && positionY_ == n_pos.getPositionY();
+};
 
-// size_t Coordinates::distance(const Coordinates& lhs, const Coordinates& rhs)
-// {
-//     return std::round(std::sqrt(std::pow((static_cast<int>(lhs.getPositionX()) - static_cast<int>(rhs.getPositionX())),
-//                                     2)
-//         + std::pow((static_cast<int>(lhs.getPositionY()) - static_cast<int>(rhs.getPositionY())),
-//             2)));
-// }
+size_t Coordinates::distance(const Coordinates& lhs, const Coordinates& rhs)
+{
+    return std::round(std::sqrt(std::pow((static_cast<int>(lhs.getPositionX()) - static_cast<int>(rhs.getPositionX())),
+                                    2)
+        + std::pow((static_cast<int>(lhs.getPositionY()) - static_cast<int>(rhs.getPositionY())),
+            2)));
+}
