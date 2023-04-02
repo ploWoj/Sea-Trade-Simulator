fd3052fb58d6080eba7904ff8f05f2784f62fff5
Wojtek <wojtek.plociennik@gmail.com>
2023-04-02 03:36:47 -0700

    implementation and change code

--- src/fruit.cpp
+++ src/fruit.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 #include <limits>
 
-Fruit::Fruit(const std::string &name, size_t amount, size_t basePrice, size_t expirationDate, std::shared_ptr<Time> time) : Cargo(name, amount, basePrice, time), expirationDate_(expirationDate){
-};
+Fruit::Fruit(const std::string &name, size_t amount, size_t basePrice, size_t expirationDate, std::shared_ptr<Time> time) : Cargo(name, amount, basePrice, time), expirationDate_(expirationDate){};
 
 Fruit::Fruit(const std::string &name, size_t amount, size_t basePrice, size_t expirationDate) : Cargo(name, amount, basePrice), expirationDate_(expirationDate) {}
 
@@ -14,7 +13,7 @@ size_t Fruit::getPrice() const
     return static_cast<size_t>(basePrice_ * static_cast<float>(purchaseDate_) / expirationDate_);
 }
 
-Fruit& Fruit::operator--()
+Fruit &Fruit::operator--()
 {
     if (purchaseDate_ <= 0)
     {
@@ -22,6 +21,7 @@ Fruit& Fruit::operator--()
     }
     purchaseDate_--;
     return *this;
+    
 }
 
 void Fruit::nextDay()
@@ -32,18 +32,21 @@ void Fruit::nextDay()
     }
 }
 
-std::shared_ptr<Cargo> Fruit::clone(const size_t &amount) const {
+std::shared_ptr<Cargo> Fruit::clone(const size_t &amount) const
+{
     return std::make_shared<Fruit>(this->getName(),
-                                    amount,
-                                    this->getBasePrice(),
-                                    this->getExpirationDate(),
-                                    time_);
+                                   amount,
+                                   this->getBasePrice(),
+                                   this->getExpirationDate(),
+                                   time_);
 }
 
-void Fruit::setTime(const std::shared_ptr<Time>& time) {
+void Fruit::setTime(const std::shared_ptr<Time> &time)
+{
     time_ = time;
 }
 
-void Fruit::detachingObserver() {
+void Fruit::detachingObserver()
+{
     time_->detachObserver(this);
 }
