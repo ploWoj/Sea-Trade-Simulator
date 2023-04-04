fefd40fa47071972e0da47d6fee5a33d64623abf
Wojtek <wojtek.plciennik@gmail.com>
2023-04-04 00:42:58 -0700

    small change

--- main.cpp
+++ main.cpp
@@ -19,9 +19,7 @@ constexpr size_t game_days = 100;
 constexpr size_t final_goal = 2'000'000;
 
 int main() {
-
     Game game(start_money, game_days, final_goal);
     game.startGame();
-
     return 0;
 }
